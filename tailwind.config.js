45c8a579ee1ab5a2186621b37cb2b64bf8c0d6d4
BlueOrang3 <matealtunashvili@gmail.com>
2023-05-15 17:34:03 +0400

    style: theme final touches

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,30 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      xs: '375px',
+      sm: '600px',
+      md: '900px',
+      lg: '1200px',
+      xl: '1536px',
+    },
+    fontFamily: {
+      sans: ['Arial', 'sans-serif'],
+      serif: ['Garamond', 'serif'],
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-      screens: {
-        xs: '375px',
-        sm: '600px',
-        md: '900px',
-        lg: '1200px',
-        xl: '1536px',
+      colors: {
+        blue: {
+          500: '#1a73e8',
+        },
+      },
+      spacing: {
+        128: '32rem',
       },
     },
   },
